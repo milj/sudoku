9b445844888f9814d07a5ee2659ee2699587259d
Milosz Jerkiewicz <milj@users.noreply.github.com>
2017-06-13 09:26:07 +0200

    WIP add peer computation

--- ruby/solver.rb
+++ ruby/solver.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+require 'set'
+
+# returns puzzle string (81 characters, 1..9 or dots for unknowns)
 def load_puzzle
   puzzle_filename = ARGV[0]
   puzzle_str = File.read(puzzle_filename)
@@ -17,8 +20,61 @@ def print_puzzle(puzzle)
   end.join("\n------+-------+------\n")
 end
 
+# returns row peers of element in the position i in puzzle string (i in 0..80)
+def row_peers(puzzle, i)
+  row_number = i / 9
+  peers = Set.new
+  ((row_number * 9)...((row_number + 1) * 9))
+    .select { |j| j != i }
+    .map { |j| puzzle[j] }
+    .select { |digit| digit != '.' }
+    .each { |digit| peers.add(digit) }
+  peers
+end
+
+def column_peers(puzzle, i)
+  column_number = i % 9
+  peers = Set.new
+  (0...9)
+    .map { |j| j * 9 + column_number }
+    .select { |j| j != i }
+    .map { |j| puzzle[j] }
+    .select { |digit| digit != '.' }
+    .each { |digit| peers.add(digit) }
+  peers
+end
+
+def square_peers(puzzle, i)
+  square_coordinates = ->(i) { [i / 27, (i % 9) / 3] }  # row 0..2, column 0..2
+  peers = Set.new
+  (0..80)
+    .select { |j| square_coordinates.call(j) == square_coordinates.call(i) }
+    .select { |j| j != i }
+    .map { |j| puzzle[j] }
+    .select { |digit| digit != '.' }
+    .each { |digit| peers.add(digit) }
+  peers
+end
+
+def peers(puzzle, i)
+  row_peers(puzzle, i) + column_peers(puzzle, i) + square_peers(puzzle, i)
+end
+
+# returns numbers which can be written into position i
+def possibilities(puzzle, i)
+  Set.new('1'..'9') - peers(puzzle, i)
+end
+
+def solved?(puzzle)
+  puzzle.scan(/[0-9]/).length == 81
+end
+
 def solve(puzzle)
-  puzzle
+  if solved?(puzzle)
+    puzzle
+  else
+    puzzle
+  end
 end
 
 puzzle = load_puzzle
