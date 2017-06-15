2d20dc0f34eb6475587b8b00e690dfa849dd3f5d
Milosz Jerkiewicz <milj@users.noreply.github.com>
2017-06-15 17:21:38 +0200

    Working Ruby solver in procedural style

--- ruby/solver.rb
+++ ruby/solver.rb
@@ -6,7 +6,7 @@ require 'set'
 def load_puzzle
   puzzle_filename = ARGV[0]
   puzzle_str = File.read(puzzle_filename)
-  #TODO handle read error
+  # TODO: handle read error
   puzzle_str.gsub(/[^0-9\.]/, '')
 end
 
@@ -20,61 +20,64 @@ def print_puzzle(puzzle)
   end.join("\n------+-------+------\n")
 end
 
-# returns row peers of element in the position i in puzzle string (i in 0..80)
+# returns set containing digits of the puzzle on given positions, without the digit on the current position
+def digits_on_positions(puzzle, positions, current_position)
+  positions
+    .reject { |p| p == current_position }
+    .map { |p| puzzle[p] }
+    .reject { |digit| digit == '.' }
+    .to_set
+end
+
+# returns row peers of digit in the position i in puzzle string (i in 0..80)
 def row_peers(puzzle, i)
   row_number = i / 9
-  peers = Set.new
-  ((row_number * 9)...((row_number + 1) * 9))
-    .select { |j| j != i }
-    .map { |j| puzzle[j] }
-    .select { |digit| digit != '.' }
-    .each { |digit| peers.add(digit) }
-  peers
+  digits_on_positions(
+    puzzle,
+    ((row_number * 9)...((row_number + 1) * 9)),
+    i
+  )
 end
 
+# returns column peers of element in the position i in puzzle string (i in 0..80)
 def column_peers(puzzle, i)
   column_number = i % 9
-  peers = Set.new
-  (0...9)
-    .map { |j| j * 9 + column_number }
-    .select { |j| j != i }
-    .map { |j| puzzle[j] }
-    .select { |digit| digit != '.' }
-    .each { |digit| peers.add(digit) }
-  peers
+  digits_on_positions(
+    puzzle,
+    (0...9).map { |p| p * 9 + column_number },
+    i
+  )
 end
 
+# returns 3x3 square peers of element in the position i
 def square_peers(puzzle, i)
-  square_coordinates = ->(i) { [i / 27, (i % 9) / 3] }  # row 0..2, column 0..2
-  peers = Set.new
-  (0..80)
-    .select { |j| square_coordinates.call(j) == square_coordinates.call(i) }
-    .select { |j| j != i }
-    .map { |j| puzzle[j] }
-    .select { |digit| digit != '.' }
-    .each { |digit| peers.add(digit) }
-  peers
+  square_coordinates = ->(p) { [p / 27, (p % 9) / 3] } # returns tuple [row 0..2, column 0..2]
+  digits_on_positions(
+    puzzle,
+    (0..80).select { |p| square_coordinates.call(p) == square_coordinates.call(i) },
+    i
+  )
 end
 
 def peers(puzzle, i)
   row_peers(puzzle, i) + column_peers(puzzle, i) + square_peers(puzzle, i)
 end
 
-# returns numbers which can be written into position i
-def possibilities(puzzle, i)
+# returns set of numbers which can be written into position i of the puzzle
+def options(puzzle, i)
   Set.new('1'..'9') - peers(puzzle, i)
 end
 
-def solved?(puzzle)
-  puzzle.scan(/[0-9]/).length == 81
-end
-
 def solve(puzzle)
-  if solved?(puzzle)
-    puzzle
-  else
-    puzzle
+  first_empty_position = puzzle.index('.')
+  return puzzle unless first_empty_position
+  options(puzzle, first_empty_position).each do |option|
+    updated_puzzle = puzzle.dup
+    updated_puzzle[first_empty_position] = option
+    solution = solve(updated_puzzle)
+    return solution if solution
   end
+  nil
 end
 
 puzzle = load_puzzle
