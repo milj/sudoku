59f2cf30dfd90d24a89dde2b61d32bf1613cc82b
Milosz Jerkiewicz <milj@users.noreply.github.com>
2017-06-19 09:06:21 +0200

    Add Board class, refactoring

--- ruby/solver.rb
+++ ruby/solver.rb
@@ -1,85 +1,103 @@
 #!/usr/bin/env ruby
+# Usage: ./solver2.rb path/to/puzzle.txt
 
 require 'set'
 
-# returns puzzle string (81 characters, 1..9 or dots for unknowns)
+class Board
+  attr_accessor :digits # a string holding the board state
+
+  def initialize(digits)
+    @digits = digits
+  end
+
+  # returns set of numbers which can be written into position i of the sudoku board
+  def options(i)
+    Set.new('1'..'9') - peers(i)
+  end
+
+  def first_empty_position
+    digits.index('.')
+  end
+
+  def to_s
+    digits.scan(/.{27}/).map do |x|
+      x.scan(/.{9}/).map do |y|
+        y.scan(/.{3}/).map do |z|
+          z.split('').join(' ')
+        end.join(' | ')
+      end.join("\n")
+    end.join("\n------+-------+------\n")
+  end
+
+  private
+
+  def peers(i)
+    row_peers(i) + column_peers(i) + box_peers(i)
+  end
+
+  # returns row peers of digit in the position i (i in 0..80)
+  def row_peers(i)
+    row_number = i / 9
+    digits_on_positions(
+      row_number * 9...(row_number + 1) * 9,
+      i
+    )
+  end
+
+  # returns column peers of element in the position i (i in 0..80)
+  def column_peers(i)
+    column_number = i % 9
+    digits_on_positions(
+      (0...9).map { |p| p * 9 + column_number },
+      i
+    )
+  end
+
+  # returns 3x3 box peers of element in the position i (i in 0..80)
+  def box_peers(i)
+    box_coordinates = ->(p) { [p / 27, (p % 9) / 3] } # returns tuple [row 0..2, column 0..2]
+    digits_on_positions(
+      (0..80).select { |p| box_coordinates.call(p) == box_coordinates.call(i) },
+      i
+    )
+  end
+
+  # returns set containing digits on given board positions, without the digit on the current position
+  def digits_on_positions(positions, current_position)
+    positions
+      .reject { |p| p == current_position }
+      .map { |p| digits[p] }
+      .reject { |digit| digit == '.' }
+      .to_set
+  end
+end
+
+# loads puzzle string (81 characters, 1..9 or dots for unknowns) from a file
 def load_puzzle
   puzzle_filename = ARGV[0]
+  raise 'No puzzle file given' unless puzzle_filename
   puzzle_str = File.read(puzzle_filename)
-  # TODO: handle read error
   puzzle_str.gsub(/[^0-9\.]/, '')
 end
 
-def print_puzzle(puzzle)
-  puzzle.scan(/.{27}/).map do |x|
-    x.scan(/.{9}/).map do |y|
-      y.scan(/.{3}/).map do |z|
-        z.split('').join(' ')
-      end.join(' | ')
-    end.join("\n")
-  end.join("\n------+-------+------\n")
-end
-
-# returns set containing digits of the puzzle on given positions, without the digit on the current position
-def digits_on_positions(puzzle, positions, current_position)
-  positions
-    .reject { |p| p == current_position }
-    .map { |p| puzzle[p] }
-    .reject { |digit| digit == '.' }
-    .to_set
-end
-
-# returns row peers of digit in the position i in puzzle string (i in 0..80)
-def row_peers(puzzle, i)
-  row_number = i / 9
-  digits_on_positions(
-    puzzle,
-    ((row_number * 9)...((row_number + 1) * 9)),
-    i
-  )
-end
-
-# returns column peers of element in the position i in puzzle string (i in 0..80)
-def column_peers(puzzle, i)
-  column_number = i % 9
-  digits_on_positions(
-    puzzle,
-    (0...9).map { |p| p * 9 + column_number },
-    i
-  )
-end
-
-# returns 3x3 square peers of element in the position i
-def square_peers(puzzle, i)
-  square_coordinates = ->(p) { [p / 27, (p % 9) / 3] } # returns tuple [row 0..2, column 0..2]
-  digits_on_positions(
-    puzzle,
-    (0..80).select { |p| square_coordinates.call(p) == square_coordinates.call(i) },
-    i
-  )
-end
-
-def peers(puzzle, i)
-  row_peers(puzzle, i) + column_peers(puzzle, i) + square_peers(puzzle, i)
-end
-
-# returns set of numbers which can be written into position i of the puzzle
-def options(puzzle, i)
-  Set.new('1'..'9') - peers(puzzle, i)
-end
-
-def solve(puzzle)
-  first_empty_position = puzzle.index('.')
-  return puzzle unless first_empty_position
-  options(puzzle, first_empty_position).each do |option|
-    updated_puzzle = puzzle.dup
-    updated_puzzle[first_empty_position] = option
-    solution = solve(updated_puzzle)
+# returns a Board instance containing a solved puzzle
+def solve(board)
+  position = board.first_empty_position
+  return board unless position
+  board.options(position).each do |option|
+    solution = solve(Board.new(
+      board.digits[0...position] + option + board.digits[position + 1..-1]
+    ))
     return solution if solution
   end
   nil
 end
 
-puzzle = load_puzzle
-
-puts print_puzzle(solve(puzzle))
+begin
+  board = Board.new(load_puzzle)
+  puts solve(board)
+rescue RuntimeError => e
+  puts e.message
+rescue Errno::ENOENT
+  puts 'Error loading the puzzle file'
+end
